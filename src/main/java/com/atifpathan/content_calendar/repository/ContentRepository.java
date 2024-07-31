7251fc6bc03e4bea9a730ffd8e42f3e042471d27
Atif Pathan <atifkhan.p1@gmail.com>
2024-07-31 11:04:02 -0600

    update ContentRepository.java

--- src/main/java/com/atifpathan/content_calendar/repository/ContentRepository.java
+++ src/main/java/com/atifpathan/content_calendar/repository/ContentRepository.java
@@ -5,17 +5,18 @@ import com.atifpathan.content_calendar.model.Status;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
+@Repository
 public interface ContentRepository extends ListCrudRepository<Content, Integer> {
 
     List<Content> findAllByTitleContains(String keyword); // using query derivation --> CHECK DOCS FOR THIS
 
-    @Query("""
-            SELECT * FROM Content
-            where status = :status
-            """)
-    List<Content> listByStatus(@Param("status") Status status);
+//    @Query("""
+//            SELECT * FROM Content
+//            where status = :status
+//            """)
+//    List<Content> listByStatus(@Param("status") Status status);
 }
